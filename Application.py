0c546a5c751e6625e978350662513044c8de6cdc
Stefan Obermeier <stefan.obermeier@tum.de>
2020-12-24 00:03:58 +0100

    Solving
    Basic solving

--- Application.py
+++ Application.py
@@ -6,6 +6,7 @@ from ocr_core import ocr_core
 
 # import OCR to hexdump functionality
 import ocr_to_hexdump
+import solver
 
 # define a folder to store and later serve the images
 UPLOAD_FOLDER = '\\temp'
@@ -35,22 +36,65 @@ def upload_page():
     if request.method == 'POST':
         # check if manual input is present
         if 'seq' in request.form:
-            '''
-            row0 = request.form['row0']
-            row1 = request.form['row1']
-            row2 = request.form['row2']
-            row3 = request.form['row3']
-            row4 = request.form['row4']
-            row5 = request.form['row5']
-            rows = ocr_to_hexdump.convert_rows_to_hexdump([row0, row1, row2, row3, row4, row5])[0]
-            return render_template('upload.html', msg='Manual Input processed', extraction_success=1,
-                                   row0=rows[0],
-                                   row1=rows[1],
-                                   row2=rows[2],
-                                   row3=rows[3],
-                                   row4=rows[4],
-                                   row5=rows[5])
-            '''
+            print("Processing Manual User Input...")
+
+            print(request.form)
+
+            seq = request.form['seq']
+            ram = request.form['ram']
+
+            e00 = request.form['e00']
+            e01 = request.form['e01']
+            e02 = request.form['e02']
+            e03 = request.form['e03']
+            e04 = request.form['e04']
+            e05 = request.form['e05']
+
+            e10 = request.form['e10']
+            e11 = request.form['e11']
+            e12 = request.form['e12']
+            e13 = request.form['e13']
+            e14 = request.form['e14']
+            e15 = request.form['e15']
+
+            e20 = request.form['e20']
+            e21 = request.form['e21']
+            e22 = request.form['e22']
+            e23 = request.form['e23']
+            e24 = request.form['e24']
+            e25 = request.form['e25']
+
+            e30 = request.form['e30']
+            e31 = request.form['e31']
+            e32 = request.form['e32']
+            e33 = request.form['e33']
+            e34 = request.form['e34']
+            e35 = request.form['e35']
+
+            e40 = request.form['e40']
+            e41 = request.form['e41']
+            e42 = request.form['e42']
+            e43 = request.form['e43']
+            e44 = request.form['e44']
+            e45 = request.form['e45']
+
+            e50 = request.form['e50']
+            e51 = request.form['e51']
+            e52 = request.form['e52']
+            e53 = request.form['e53']
+            e54 = request.form['e54']
+            e55 = request.form['e55']
+
+            hexdump = [[e00, e01, e02, e03, e04, e05],
+                       [e10, e11, e12, e13, e14, e15],
+                       [e20, e21, e22, e23, e24, e25],
+                       [e30, e31, e32, e33, e34, e35],
+                       [e40, e41, e42, e43, e44, e45],
+                       [e50, e51, e52, e53, e54, e55]
+                       ]
+            print("Hexdump fetched")
+            solution = solver.solve(hexdump, seq, ram)
+            return render_template('upload.html', msg='Processing Complete', solution=solution)
 
         # check if there is a file in the request
         if 'file' not in request.files:
@@ -70,13 +114,6 @@ def upload_page():
             extracted_text = ocr_core(file)
             rows_and_hexdump = ocr_to_hexdump.convert_ocr_to_hexdump(extracted_text)
             rows = rows_and_hexdump[0]
-
-            '''
-            hexdump = rows_and_hexdump[1]
-            ocr_to_hexdump.print_hexdump(hexdump)
-            for row in rows:
-                print(row)
-            '''
 
             # extract the text and display it
             return render_template('upload.html',
---
+++ solver.py
@@ -0,0 +1,103 @@
+import ocr_to_hexdump
+import copy
+
+
+def validate_hexdump(hexdump):
+    valid_hex = ['55', '7A', 'BD', '1C', 'E9']
+    for col in hexdump:
+        for row in col:
+            if row not in valid_hex:
+                print("Invalid Hex: {}".format(row))
+                return -1
+
+
+def text_to_sequence(text):
+    seq = []
+    for i in range(0, int(len(text) / 2)):
+        seq.append(text[0:2])
+        text = text[2:len(text)]
+    return seq
+
+
+def find_all_possible_combinations(hexdump, ram):
+    all_possible_combinations = []
+
+    hexdump_loc = copy.deepcopy(hexdump)
+
+    for column in range(0, len(hexdump_loc)):
+        hex = hexdump_loc[column][0]
+        hexdump_loc[column][0] = '--'
+        find_combination_recursive(all_possible_combinations, hexdump_loc, ram - 1, hex, [column, 0], 'vert')
+
+    return all_possible_combinations
+
+
+def find_combination_recursive(all_possible_combinations, hexdump_rec, ram_left, seq, last_position, vert_horz):
+    # print(seq)
+
+    hexdump_loc = copy.deepcopy(hexdump_rec)
+    hexdump_loc[last_position[0]][last_position[1]] = '--'
+
+    if ram_left == 0:
+        all_possible_combinations.append(seq)
+        return
+
+    if vert_horz == 'vert':
+        column = last_position[0]
+        for row in range(0, 6):
+            if row != last_position[1] and hexdump_loc[column][row] != '--':
+                find_combination_recursive(all_possible_combinations, hexdump_loc, ram_left - 1,
+                                           seq + hexdump_loc[column][row],
+                                           [column, row], 'horz')
+    elif vert_horz == 'horz':
+        row = last_position[1]
+        for column in range(0, 6):
+            if column != last_position[0] and hexdump_loc[column][row] != '--':
+                find_combination_recursive(all_possible_combinations, hexdump_loc, ram_left - 1,
+                                           seq + hexdump_loc[column][row],
+                                           [column, row], 'vert')
+
+
+def solve(hexdump, seq_text, ram):
+    print("Validating Hexdump:")
+    validate_hexdump(hexdump)
+    print("Hexdump valid!")
+    ram = int(ram)
+    print("Attempt to solve:")
+    ocr_to_hexdump.print_hexdump(hexdump)
+    print("\nFind sequence {} with {} RAM".format(seq_text, ram))
+
+    seq = text_to_sequence(seq_text)
+
+    print(seq)
+    if ram < len(seq):
+        print("Insufficient RAM")
+        return -1
+    else:
+        print("RAM could be sufficient")
+
+    print("Calculate all possible sequences using {} RAM".format(ram))
+    all_possible_combinations = find_all_possible_combinations(hexdump, ram)
+    print("All possible sequences calculated!")
+
+    print("Trying to find Sequence {}".format(seq_text))
+    for combination in all_possible_combinations:
+        if combination.find(seq_text) != -1:
+            print(text_to_sequence(combination))
+            return combination
+
+    return 'No solution could be found :('
+
+
+'''
+hexdump = [['55', '1C', '55', 'BD', '1C', '7A'], ['7A', 'BD', '1C', '55', 'E9', '1C'],
+           ['BD', '55', 'BD', '55', '55', '1C'], ['1C', '55', '1C', 'BD', '55', 'BD'],
+           ['55', 'E9', '7A', '1C', '1C', '1C'], ['7A', 'BD', '1C', '55', '55', 'BD']]
+
+validate_hexdump(hexdump)
+ocr_to_hexdump.print_hexdump(hexdump)
+
+solve(hexdump, '7ADBE91C', 8)
+
+print("")
+'''
